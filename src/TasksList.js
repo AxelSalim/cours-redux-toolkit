8a0a1da81887c8fa8ca64ea6269e5b74856b945f
AxelSalim <adjakidjememiaghe@gmail.com>
2024-10-17 20:34:10 +0100

    implémentation de la partie TasksList.js

--- src/TasksList.js
+++ src/TasksList.js
@@ -1,16 +1,19 @@
+ import { useSelector } from "react-redux";
 import TaskItem from "./TaskItem";
 
 const TasksList = (props) => {
+  const tasks = useSelector((state) => state.tasks)
+  console.log(tasks)
   return (
     <>
-      {props.tasks.map((t) => (
+      {/* {props.tasks.map((t) => (
         <TaskItem
           task={t}
           key={t.id}
           toggleTask={props.toggleTask}
           deleteTask={props.deleteTask}
         />
-      ))}
+      ))} */}
     </>
   );
 };
