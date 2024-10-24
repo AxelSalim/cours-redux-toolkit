ef3689c718fe3e06d8babc9ce5a42cf278357a7c
AxelSalim <adjakidjememiaghe@gmail.com>
2024-10-24 11:09:09 +0100

    modification du app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import "./styles.css";
 import TaskForm from "./TaskForm";
-import TasksHeader from "./TasksHeader";
 import TasksList from "./TasksList";
 import { Provider } from "react-redux";
 import { store } from "./redux";
